b45b39407e2af304cacf921b5304fd966ad5e262
gdrtf <gdrtf@protonmail.com>
2022-01-26 15:18:02 +0900

    [Back] Add google oauth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
+const cookieSession = require("cookie-session");
 const express = require("express");
+const cors = require("cors");
+const passport = require("passport");
+const authRoute = require("./routes/auth");
 const app = express();
 const SERVER_PORT = 3001;
 
-app.get("/api", (_, res) => {
-    res.json({ message: "How you doin?" });
-});
+app.use(
+  cookieSession({
+      name: "session",
+      keys: ["lama"],
+      maxAge: 8640000
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(
+  cors({
+      origin: "http://localhost:3000",
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true,
+  })
+);
+
+app.use("/auth", authRoute);
 
 app.listen(SERVER_PORT, () => {
   console.log(`Listening on ${SERVER_PORT}`);
