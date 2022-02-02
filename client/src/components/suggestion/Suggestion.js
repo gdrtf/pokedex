53f616c9e8f5794c4e3f3decfe3ec4a57f00b22b
gdrtf <gdrtf@protonmail.com>
2022-02-02 12:10:31 +0900

    [Desgin] Bold typeahead in suggestions

--- client/src/components/suggestion/Suggestion.js
+++ client/src/components/suggestion/Suggestion.js
@@ -1,8 +1,19 @@
-export default function Suggestion({suggestion}) {
+export default function Suggestion({suggestion, typeahead}) {
+  if (typeahead == null || typeahead.length == 0) {
+    return (
+      <div>
+        {suggestion}
+      </div>
+    )
+  } else {
+    const index = suggestion.indexOf(typeahead);
+    const prefix = suggestion.substring(0, index);
+    const suffix = suggestion.substring(index + typeahead.length);
 
-  return (
-    <div>
-      {suggestion}
-    </div>
-  )
+    return (
+      <div>
+        {prefix}<strong>{typeahead}</strong>{suffix}
+      </div>
+    )
+  }
 }
