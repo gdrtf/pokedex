d8db7ec373ccfb3aa44e77ae24596d5ee9eded83
gdrtf <gdrtf@protonmail.com>
2022-02-02 11:57:32 +0900

    [Design] Better spacing

--- client/src/components/card/Card.js
+++ client/src/components/card/Card.js
@@ -10,22 +10,17 @@ export default function Card({pokemon}) {
         <div className='column'>
           <div className='line'>
             {pokemon.front ? <img src={pokemon.front} alt="" /> : <div />}
-            {/* {pokemon.back ? <img src={pokemon.back} alt="" /> : <div />} */}
           </div>
-          {/* <div className='line'>
-            <div className="label">{pokemon.height}m</div>
-            <div className="label">{pokemon.weight}kg</div>
-          </div> */}
         </div>
         <div className='column'>
           <div className="label name"><h1>{pokemon.name}</h1></div>
-            <div className='line'>
-              <div className='types'>
-                {pokemon.types.map(type => {
-                  return <Type key={type} type={type} />
-                })}
-              </div>
-            <div className="label">id: {pokemon.id}</div>
+          <div className="even-line">
+            <div className='types'>
+              {pokemon.types.map(type => {
+                return <Type key={type} type={type} />
+              })}
+            </div>
+            <div className="id">id: {pokemon.id}</div>
           </div>
         </div>
       </div>
